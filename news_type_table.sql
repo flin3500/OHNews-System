e0d0d03366229d300b90ff4083e8de805a271ce0
Lin <lin.acandc@gmail.com>
2020-08-11 14:30:39 +1000

    change table

--- news_type_table.sql
+++ news_type_table.sql
@@ -3,5 +3,5 @@ USE galaxy;
 CREATE TABLE news_type(
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	type VARCHAR(20) NOT NULL UNIQUE
-)
+);
 INSERT INTO news_type(type) VALUE("Finance"),("World"),("National"),("Technology");
