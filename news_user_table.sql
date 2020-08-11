1984acd4318c62e691cedd8552abbd89c8d69662
Lin <lin.acandc@gmail.com>
2020-08-11 14:37:26 +1000

    add news user table

---
+++ news_user_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE news_user(
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(20) NOT NULL UNIQUE,
+	password VARCHAR(500) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	role_id INT UNSIGNED NOT NULL,
+	INDEX(username)
+);
+INSERT INTO news_user(username, password, email, role_id)
+VALUE("Admin",HEX(AES_ENCRYPT("123456","HelloWorld")), "admin@gmail.com", 1);
